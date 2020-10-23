383b1b94c81af5520310ba66f3aa19a927c36fac
jackson parker <71060409+j2ckson@users.noreply.github.com>
2020-10-22 23:55:41 -0700

    Update timeDiff.c

--- src/timeDiff.c
+++ src/timeDiff.c
@@ -155,7 +155,7 @@ void time_diff(timeD xx, double rStat[], int em[], struct timespec exestart, str
 			printf(DEFCOLOR);
 			//printcc("", 0, xx.colour);
 			printcc("\n", 0, 0);
-			printf(DEFCOLOR);
+			printf(HEADER);
 			for ( int i=0;i<rowwidth-1;i++ ) {
 				printcc(" ", 4, xx.colour);
 			}
